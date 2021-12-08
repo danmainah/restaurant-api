3c2b5214e6f6c714676925c31eca2db9ab22b66d
danmainah <danielmainawaweru@gmail.com>
2021-12-08 11:33:38 +0300

    Add user route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    devise_for :users,
+               controllers: {
+                   sessions: 'api/v1/users/sessions',
+                   registrations: 'api/v1/users/registrations'
+               }
+    get '/api/v1/member-data', to: 'members#show'
+  end
 end
