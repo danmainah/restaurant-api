36b5977db8a5e428bfab39c9af597fed927b0d2d
danmainah <danielmainawaweru@gmail.com>
2021-12-17 11:41:19 +0300

    Fix authorization issue

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,7 +4,7 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
-    render json: { message: 'You are logged in.' }, status: :ok
+    render json: { message: 'You are logged in.' , access_token: response.headers['Authorization'] }, status: :ok
   end
 
   def respond_to_on_destroy
@@ -14,7 +14,7 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def log_out_success
-    render json: { message: "You are logged out." }, status: :ok
+    render json: { message: "You are logged out."}, status: :ok
   end
 
   def log_out_failure
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,8 +9,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '*', headers: %w(Authorization),
+                  methods: :any,
+                  expose: %w[Authorization]
   end
 end
